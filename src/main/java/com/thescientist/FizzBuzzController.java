5be0fc83c197f7dddc0d399464c6b4863561da98
Welliton Paiva <wellitonpaiva@gmail.com>
2019-05-05 13:03:07 -0300

    Initial commit

---
+++ src/main/java/com/thescientist/FizzBuzzController.java
@@ -0,0 +1,24 @@
+package com.thescientist;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+
+@RestController
+public class FizzBuzzController {
+
+    @RequestMapping(value = "/fizzbuzz/{number}", method = RequestMethod.GET)
+    public @ResponseBody String index(@PathVariable int number) {
+        if (number % 15 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        }
+        return Integer.toString(number);
+    }
+}
